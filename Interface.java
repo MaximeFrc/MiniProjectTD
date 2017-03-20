136f989660d78cf7301a05868c93faa20a4547a2
Unknown <maxime.francois@insa-lyon.fr>
2017-03-20 09:15:59 +0100

    test

--- Interface.java
+++ Interface.java
@@ -162,7 +162,6 @@ public class Interface extends JPanel implements ActionListener,MouseListener{
 		}
 		
 		if (e.getSource() == deleteTower) {
-			// delete the tower a the next position clicked
 			description.setText("<html> DELETE TOWER : <br> If you click on a tower <br> 80 % of the initial price <br> will be refund. </html>");	
 			this.isDeleteTower=true;
 			deleteTower.setBackground(new Color(30,55,112));
